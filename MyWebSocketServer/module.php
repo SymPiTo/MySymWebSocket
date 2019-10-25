98796973e0feb20995ec213e978c2d4b6677c500
Your NameSymPiTo <pito@online.de>
2019-10-25 21:49:59 +0200

    g

--- MyWebSocketServer/module.php
+++ MyWebSocketServer/module.php
@@ -1381,7 +1381,7 @@ class MyWebsocketServer extends IPSModule
                     //TB 21.10.2019 egänzt entferne Client wenn nicht verbunden
                     $this->SendDebug('Entferne Client da nicht verbunden: ', $ClientIP . ':' . $ClientPort, 0);
                     $this->RemoveClient($Client);
-                    $x = this->GetClients();
+                    $x = $this->GetClients();
                     $this->SendDebug('bereinigte Client Liste: ',  $x, 0);
                     //$this->RestartServer();
                     //return false;
