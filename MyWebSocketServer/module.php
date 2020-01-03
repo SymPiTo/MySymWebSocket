79fe8bd5e1fedb6f66938d1469648127567e15bc
GitIPS <torsten.beck@@onlinehome.de>
2020-01-03 17:51:37 +0100

    new Version

--- MyWebSocketServer/module.php
+++ MyWebSocketServer/module.php
@@ -583,7 +583,7 @@ class MyWebsocketServer extends IPSModule
      * @param type $UseTLS Bei false wird TLS nicht benutzt, auch wenn der Client dies erwartet.
      * @return boolean|string Der JSON-String zum versand an den IO, im Fehlerfall false.
      */
-    private function MakeJSON(Websocket_Client $Client, string $Data, int $Type = SocketType::Data)
+    private function MakeJSON(Websocket_Client $Client, string $Data, $UseTLS = true, int $Type = SocketType::Data)
     {
         if ($Type == SocketType::Data) {
             if ($UseTLS and $Client->UseTLS) {
