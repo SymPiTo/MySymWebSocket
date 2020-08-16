0044950db1e2365cb04645dac14acbcc1afebbcc
GitIPS <torsten.beck@@onlinehome.de>
2020-08-16 20:10:51 +0200

    ff

--- MyWebSocketServer/module.php
+++ MyWebSocketServer/module.php
@@ -928,7 +928,7 @@ class MyWebsocketServer extends IPSModule
                 $this->lock($Client->ClientIP . $Client->ClientPort);
                 $TLS = \PTLS\TLSContext::createTLS($TLSconfig);
             }
-            if ($this->UsePlain and ( preg_match("/^GET ?-* HTTP\/1.1\r\n/", $Payload, $match))) { //valid header wenn Plain is active
+            if ($this->UsePlain and ( preg_match("/^GET ?.* HTTP\/1.1\r\n/", $Payload, $match))) { //valid header wenn Plain is active
                 $Client->State = WebSocketState::HandshakeReceived;
                 $this->SendDebug('Receive'.'Handshake Receives',$match, 0);
                 $Client->UseTLS = false;
