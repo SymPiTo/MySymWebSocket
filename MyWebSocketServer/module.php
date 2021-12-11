68caebb3f1e91d971b257bdc8403e6570aac4276
GitIPS <torsten.beck@@onlinehome.de>
2021-12-11 15:41:40 +0100

    wrw

--- MyWebSocketServer/module.php
+++ MyWebSocketServer/module.php
@@ -1492,7 +1492,7 @@ class MyWebsocketServer extends IPSModule
         $Clients = $this->Multi_Clients;
         $cl = $Clients->GetClients();
         //$Clients = $this->Multi_Clients->GetClients();
-        $this->SendDebug('SendText Funktion wird ausgeführt. ', json_decode($cl, true) , 0);
+        $this->SendDebug('SendText Funktion wird ausgeführt. ', $cl, 0);
         if ($Clients){
            $this->SendDebug('Client Liste =' , $Clients, 0);
 
