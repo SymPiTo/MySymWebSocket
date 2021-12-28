5cfbd199e4cff4825a2049a951589d0519b9f578
GitIPS <torsten.beck@@onlinehome.de>
2021-12-28 15:13:00 +0100

    ewqe

--- MyWebSocketServer/module.php
+++ MyWebSocketServer/module.php
@@ -1345,9 +1345,6 @@ class MyWebsocketServer extends IPSModule
         $this->SendDebug('Received following Data from Client', $Data, 0); 
  
         SetValueString(26720, $Data);
-        $this->SendDebug(json_decode($Data, true), 0);
-
-        //call_user_func_array(json_decode($Data, true));
 
         if(substr($Data, 0, 8) == 'setvalue'){
            $Data = explode(",", substr($Data, 9, strlen($Data)-10));
