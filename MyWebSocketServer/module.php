5bde466eef336c3715a4ccaeaacd5ea7ce501875
GitIPS <torsten.beck@@onlinehome.de>
2022-01-30 14:37:47 +0100

    ee

--- MyWebSocketServer/module.php
+++ MyWebSocketServer/module.php
@@ -1798,7 +1798,7 @@ class MyWebsocketServer extends IPSModule
             
         $MyVars = $this->Multi_Vars;
             $test = $MyVars->getData();
-        $this->SendDebug("sendIPSVars", get_object_vars($test), 0);
+        $this->SendDebug("sendIPSVars", $test['hash'], 0);
 
             $liste = array();
             $Clients = $this->Multi_Clients;
